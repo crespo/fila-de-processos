772b6d3ff9406f006323f53125e30a161d70899d
raul crespo <raulxcrespo@gmail.com>
2024-04-21 19:09:25 -0300

    fix logic

--- Processo.py
+++ Processo.py
@@ -1,9 +1,5 @@
 class Processo:
     def __init__(self, protocolo, nomeInteressado, assunto):
-        if protocolo and nomeInteressado and assunto:
-            self.protocolo = protocolo
-            self.nomeInteressado = nomeInteressado
-            self.assunto = assunto
-            return True
-        
-        return False
+        self.protocolo = protocolo
+        self.nomeInteressado = nomeInteressado
+        self.assunto = assunto
